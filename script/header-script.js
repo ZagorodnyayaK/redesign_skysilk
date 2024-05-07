38d99f799cb2161f8135aedf8868fb6ebb940682
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2024-05-07 19:48:13 +0200

    init

--- script/header-script.js
+++ script/header-script.js
@@ -1,23 +1,23 @@
-let maxWidth = 1500;
-let body = document.querySelector('body');
-if (window.innerWidth <= maxWidth) {
-    body.classList.add('touch');
-
-    let arrow = document.querySelectorAll('.arrow');
-
-    for (let i = 0; i < arrow.length; i++) {
-        arrow[i].addEventListener('click', function () {
-            let thisLink = arrow[i].previousElementSibling;
-            let subMenu = arrow[i].nextElementSibling;
-            let thisArrow = arrow[i];
-
-            thisLink.classList.add('parent');
-            arrow[i].addEventListener('click', function () {
-                subMenu.classList.toggle('open');
-                thisArrow.classList.toggle('active');
-            })
-        })
-    }
-} else {
-    body.classList.add('mouse');
-}
+// let maxWidth = 1500;
+// let body = document.querySelector('body');
+// if (window.innerWidth <= maxWidth) {
+//     body.classList.add('touch');
+//
+//     let arrow = document.querySelectorAll('.arrow');
+//
+//     for (let i = 0; i < arrow.length; i++) {
+//         arrow[i].addEventListener('click', function () {
+//             let thisLink = arrow[i].previousElementSibling;
+//             let subMenu = arrow[i].nextElementSibling;
+//             let thisArrow = arrow[i];
+//
+//             thisLink.classList.add('parent');
+//             arrow[i].addEventListener('click', function () {
+//                 subMenu.classList.toggle('open');
+//                 thisArrow.classList.toggle('active');
+//             })
+//         })
+//     }
+// } else {
+//     body.classList.add('mouse');
+// }
